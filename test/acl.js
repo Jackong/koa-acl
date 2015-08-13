f78d11bff64f1881ca16fe68cf56403386f81908
Jackong <JackongC@gmail.com>
2015-08-13 09:22:23 +0800

    update test for user

--- test/acl.js
+++ test/acl.js
@@ -1,40 +1,34 @@
 var expect = require('chai').expect;
-var ACL = require('../lib/acl');
-var Acl = require('acl');
+var Acl = require('../lib/acl');
 var request = require('supertest-koa-agent');
 var koa = require('koa');
 
-var acl = ACL(new Acl(new Acl.memoryBackend()));
+var acl = new Acl(new Acl.memoryBackend());
 
-ACL.onGetUser(function() {
+Acl.onGetUser(function() {
     return this.state.user.id;
 });
 
 describe('acl middleware', function() {
+
+    before(function(done) {
+        acl.allow('admin', '/api/users', 'post', done);
+    });
+    before(function(done) {
+        acl.addUserRoles('admin', 'admin', done);
+    });
+
+
     describe('with default user getter', function() {
         var app = koa();
         app.use(function* (next) {
             this.state.user = {id: this.query.id};
             yield next;
         });
-        app.use(ACL.middleware());
+        app.use(acl.middleware());
         app.use(function* (next){
             this.body = 'ok';
         });
-
-        before(function(done) {
-            acl.allow('admin', '/api/users', 'post', done);
-        });
-        before(function(done) {
-            acl.addUserRoles('admin', 'admin', done);
-        });
-        before(function(done) {
-            acl.isAllowed('admin', '/api/users', 'post', function(err, allowed) {
-                console.log(err, allowed);
-                done(err);
-            });
-        });
-
         describe('and un-authorized user', function() {
             it('should be fail', function(done) {
                 request(app)
@@ -55,15 +49,32 @@ describe('acl middleware', function() {
     });
 
     describe('with custom user getter', function() {
+        var app = koa();
+        app.use(function* (next) {
+            this.state.user = {id: this.query.id};
+            yield next;
+        });
+        app.use(acl.middleware(2, function() {
+            this.state.user = {id: this.query.cid};
+        }));
+        app.use(function* (next){
+            this.body = 'ok';
+        });
         describe('and un-authorized user', function() {
-            it('should be fail', function() {
-
+            it('should be fail', function(done) {
+                request(app)
+                    .post('/api/users?id=admin&cid=guest')
+                    .expect(403)
+                    .end(done);
             });
         });
 
         describe('and authorized user', function() {
-            it('should be success', function() {
-
+            it('should be success', function(done) {
+                request(app)
+                    .post('/api/users?id=guest&cid=admin')
+                    .expect(200, 'ok')
+                    .end(done);
             });
         });
     });
