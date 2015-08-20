409c2d6b19772e138df95e388bd6fd93422c9be3
Jackong <JackongC@gmail.com>
2015-08-20 13:29:33 +0800

    add backend tests

--- test/acl.js
+++ test/acl.js
@@ -17,59 +17,96 @@ describe('middleware', () => {
         acl.addUserRoles('jackong', 'admin', done);
     });
 
-
-    var app = koa();
-    app.use(function* (next) {
-        this.state.user = {_id: this.query.id};
-        yield next;
-    });
-    app.use(
-        ACL({
-            user: ctx => {
-                return ctx.state.user._id
-            },
-            backend: ctx => {
-                return Promise.resolve(backend)
-            }
+    describe('user', () => {
+        var app = koa();
+        before(() => {
+            app.use(function* (next) {
+                this.state.user = {_id: this.query.id};
+                yield next;
+            });
+            app.use(
+                ACL({
+                    user: ctx => {
+                        return ctx.state.user._id
+                    },
+                    backend: ctx => {
+                        return Promise.resolve(backend)
+                    }
+                })
+            );
+            app.use(ACL.middleware(2));
+            app.use(function* (next){
+                this.body = 'ok';
+            });
         })
-    );
-    app.use(ACL.middleware(2));
-    app.use(function* (next){
-        this.body = 'ok';
-    });
-    describe('with path shorter than resource', () => {
-        it('should be fail', function(done) {
-            request(app)
-                .post('/api?id=jackong')
-                .expect(403)
-                .end(done);
-        });
+
+        describe('with path shorter than resource', () => {
+            it('should be fail', function(done) {
+                request(app)
+                    .post('/api?id=jackong')
+                    .expect(403, 'Insufficient permissions to access resource')
+                    .end(done);
+            });
+        })
+
+        describe('with path same as resource', () => {
+            it('should be ok', function(done) {
+                request(app)
+                    .post('/api/users?id=jackong')
+                    .expect(200, 'ok')
+                    .end(done);
+            });
+        })
+
+        describe('with path longer than resource', () => {
+            it('should be ok', function(done) {
+                request(app)
+                    .post('/api/users/11?id=jackong')
+                    .expect(200, 'ok')
+                    .end(done);
+            });
+        })
+
+        describe('with guest user', () => {
+            it('should be fail', done => {
+                request(app)
+                    .post('/api/users/11?id=guest')
+                    .expect(403, 'Insufficient permissions to access resource')
+                    .end(done)
+            })
+        })
     })
 
-    describe('with path same as resource', () => {
-        it('should be ok', function(done) {
-            request(app)
-                .post('/api/users?id=jackong')
-                .expect(200)
-                .end(done);
-        });
-    })
+    describe('backend', () => {
+        var app = koa();
+        before(() => {
+            app.use(function* (next) {
+                this.state.user = {_id: this.query.id};
+                yield next;
+            });
+            app.use(
+                ACL({
+                    user: ctx => {
+                        return ctx.state.user._id
+                    },
+                    backend: ctx => {
+                        return Promise.reject()
+                    }
+                })
+            );
+            app.use(ACL.middleware(2));
+            app.use(function* (next){
+                this.body = 'ok';
+            });
+        })
 
-    describe('with path longer than resource', () => {
-        it('should be ok', function(done) {
-            request(app)
-                .post('/api/users/11?id=jackong')
-                .expect(200)
-                .end(done);
-        });
-    })
-
-    describe('with guest user', () => {
-        it('should be fail', done => {
-            request(app)
-                .post('/api/users/11?id=guest')
-                .expect(403)
-                .end(done)
+        describe('is rejected', () => {
+            it('should be error', done => {
+                request(app)
+                    .post('/api/users/11?id=jackong')
+                    .expect(403, 'Fail to get backend')
+                    .end(done);
+            })
         })
     })
 })
